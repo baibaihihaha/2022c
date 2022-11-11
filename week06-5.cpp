861a975ef108664b1c11dd8400ad728d20b5b432
baibaihihaha <michelle60123@gmail.com>
2022-11-11 11:51:06 +0800

    修改目錄

--- week06-5.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    for(int i=1;i<=5;i++){
-        int star=i*2-1;///有幾個星星
-        printf("鷹架:%d樓 %d星\n",i,star);
-}
---
+++ week07/week07-1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int n=1234567812345678;
+    printf("%d\n",n);///出事了
+    long long int a=1234567812345678;
+    printf("%lld\n",a);///不是數字11是英文小寫ll
+}
