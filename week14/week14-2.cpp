b419f05d99133b6100e6d061fc3210c9bfcf2e1c
baibaihihaha <michelle60123@gmail.com>
2022-12-09 12:03:36 +0800

    week14

---
+++ week14/week14-2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int a=10;
+void func()
+{
+    a=20;
+    printf("func()中a是:%d\n",a);
+    a=30;
+    printf("func()中a改成:%d\n",a);
+}
+int main()
+{
+    printf("main()中a是:%d\n",a);
+    func();
+    printf("main()中a是:%d\n",a);
+}
+
